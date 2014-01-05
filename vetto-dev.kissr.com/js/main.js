fd744c509e867d33abf2a9964e4212d685a1d561
leapische <Lea@Leas-MacBook-Air.local>
2014-01-04 23:03:25 -0800

    Reinitialize

---
+++ vetto-dev.kissr.com/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	console.log("I got in");
+
+	$('.line-1').on('click', function(){
+	    $(this).toggleClass('line-1-active');
+	});
+
+	$('.line-2').on('click', function(){
+	    $(this).toggleClass('line-2-active');
+
+	});
+
+	$('.line-3').on('click', function(){
+	    $(this).toggleClass('line-3-active');
+
+	});
+
+	$('.line-4').on('click', function(){
+	    $(this).toggleClass('line-4-active');
+
+	});
+
+	$('.home-menu').on('click', function(){
+	    $(this).toggleClass('main-line-background-active');
+	    $('.logo-left').attr("src",'img/logo-dark.png');
+	    $('.big-title-1').toggleClass('big-title-active');
+
+	});
+
+	$('.lists').on('click', function(){
+	    $(this).toggleClass('main-line-background-active');
+	    $('.nav-img').attr("src",'img/nav-icon-dark.png');
+	    $('.big-title-3').toggleClass('big-title-active');
+
+	});
+
+	$('.logout').on('click', function(){
+	    $(this).toggleClass('main-line-background-active');
+	    $('.logout-img').attr("src",'img/logout-icon-dark.png');
+	    $('.big-title-2').toggleClass('big-title-active');
+
+	});
+
+})
+
+
